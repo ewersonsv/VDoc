992b2f610bd0ee611e09e8bf43dfc510c59b99f6
Ewerson S <32661918+ewersonsv@users.noreply.github.com>
2018-08-17 10:44:57 -0300

    Update filter.php

--- function/filter.php
+++ function/filter.php
@@ -1,35 +1,26 @@
 <?php 
-	session_start();
-	require './config.php';
-	require './vdoc.php';
-
+	require './class/classe-vdoc.php';
 	$doc = filter_input(INPUT_GET, 'doc');
 	$doc = preg_replace( '/[^0-9]/', '', $doc );
 	if(isset($doc)){
 		if(strlen($doc) > 10){
-			if($doc != '' OR $doc != null){
-	 			$cpf_cnpj = new ValidaCPFCNPJ($doc);
-				// Verifica se o CPF ou CNPJ é válido
-				if ( $cpf_cnpj->valida() ) {
-					$sql_lite = "SELECT * FROM contas WHERE doc = '{$doc}'";
-					$contador = count($conn->query($sql_lite)->fetchAll());
-					if($contador > 0){
-						if(strlen($doc) > 13){ //CNPJ 14 Caracteres
-							$msgErro = 'Já Existe Conta Usando Este '.'<u>'.'CNPJ'.'</u>'.' Em Nosso Sistema...';
-							echo '<label id="cdoc-error" class="error" for="cdoc">'.$msgErro.'</label>';
-						}else{ //CPF 11 Caracteres
-							$msgErro = 'Já Existe Conta Usando Este '.'<u>'.'CPF'.'</u>'.' Em Nosso Sistema...';
-							echo '<label id="cdoc-error" class="error" for="cdoc">'.$msgErro.'</label>';
-						}
-					}
-				} else {
-					if(strlen($doc) > 13){ //CNPJ 14 Caracteres
-						$msgErro = '<u>'.'CNPJ'.'</u>'.' Inválido!';
-						echo '<label id="cdoc-error" class="error" for="cdoc">'.$msgErro.'</label>';
-					}else{ //CPF 11 Caracteres
-						$msgErro = '<u>'.'CPF'.'</u>'.' Inválido!';
-						echo '<label id="cdoc-error" class="error" for="cdoc">'.$msgErro.'</label>';
-					}
+			$cpf_cnpj = new ValidaCPFCNPJ($doc);
+			// Verifica se o CPF ou CNPJ é válido
+			if ( $cpf_cnpj->valida() ) {
+				if(strlen($doc) > 13){ //CNPJ 14 Caracteres
+					$msgErro = 'Já Existe Conta Usando Este '.'<u>'.'CNPJ'.'</u>'.' Em Nosso Sistema...';
+					echo '<label id="cdoc-error" class="error" for="cdoc">'.$msgErro.'</label>';
+				}else{ //CPF 11 Caracteres
+					$msgErro = 'Já Existe Conta Usando Este '.'<u>'.'CPF'.'</u>'.' Em Nosso Sistema...';
+					echo '<label id="cdoc-error" class="error" for="cdoc">'.$msgErro.'</label>';
+				}
+			} else {
+				if(strlen($doc) > 13){ //CNPJ 14 Caracteres
+					$msgErro = '<u>'.'CNPJ'.'</u>'.' Inválido!';
+					echo '<label id="cdoc-error" class="error" for="cdoc">'.$msgErro.'</label>';
+				}else{ //CPF 11 Caracteres
+					$msgErro = '<u>'.'CPF'.'</u>'.' Inválido!';
+					echo '<label id="cdoc-error" class="error" for="cdoc">'.$msgErro.'</label>';
 				}
 			}
 		}else{
